1f2148e21a5ab04594db3da9c6b03f7819ff2dec
Mapgie Embedded <33968140+Magpie_Embedded@users.noreply.github.com>
2019-12-12 18:28:59 +0000

    Implemented test properly

--- python/test_lz77.py
+++ python/test_lz77.py
@@ -2,7 +2,7 @@
 
 import string
 from hypothesis import given, example
-from hypothesis.strategies import text, characters
+from hypothesis.strategies import text
 
 from compress import compress, to_bytes
 from decompress import decompress, from_bytes
@@ -20,19 +20,17 @@ def test_decompress_inverts_compress(s):
     assert decompress(compress(s)) == s
 
 
-class Test_To_And_From_Bytes_For_Simple_Strings():
+class Test_To_And_From_Bytes_For_Simple_Strings:
     def test_one_character(self):
         assert decompress(from_bytes(to_bytes(compress("a")))) == "a"
 
-        
     def test_multiple_character(self):
         assert decompress(from_bytes(to_bytes(compress("abcdefg")))) == "abcdefg"
 
-        
     def test_repeated_characters(self):
         assert decompress(from_bytes(to_bytes(compress("aaaaa")))) == "aaaaa"
 
     # Only ascii at the moment
-    @given(characters(max_codepoint=8, whitelist_characters=string.printable))
+    @given(text(alphabet=string.printable, max_size=15))
     def test_simple_strings(self, s):
         assert decompress(from_bytes(to_bytes(compress(s)))) == s
